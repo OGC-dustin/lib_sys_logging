31e1b4d690f4a9d39ca1c75e07862551725655b3
dustin <dustin@ogc.engineering>
2022-11-25 16:45:59 -0600

    remove project specific aspects

--- lib_sys_logging.c
+++ lib_sys_logging.c
@@ -7,172 +7,284 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <stddef.h>
 
 #include "lib_sys_logging.h"
 #include "hal.h"
 
-const char
-lib_sys_logging_status_strings[ enum_LIB_SYS_LOGGING_STATUS_LIMIT ][ 8U ] = {
-    [ enum_LIB_SYS_LOGGING_STATUS_EMPTY ]   = "EMPTY",
-    [ enum_LIB_SYS_LOGGING_STATUS_PENDING ] = "PENDING",
-    [ enum_LIB_SYS_LOGGING_STATUS_FULL ]    = "FULL",
+#define HEADER ( 12U ) /* leave room in output for header, adjust if needed */
+
+struct s_lib_sys_logging_status lib_sys_logging_status = {
+    LIB_SYS_LOGGING_LEVEL_LOCAL,
+    NULL,
+    LIB_SYS_LOGGING_LEVEL_REMOTE,
+    NULL,
 };
 
-const char
-lib_sys_logging_level_strings[ enum_LIB_SYS_LOGGING_LEVEL_LIMIT ][ 8U ] = {
-    [ enum_LIB_SYS_LOGGING_LEVEL_NONE ]     = "NONE",
-    [ enum_LIB_SYS_LOGGING_LEVEL_FATAL ]    = "FATAL",
-    [ enum_LIB_SYS_LOGGING_LEVEL_ERROR ]    = "ERROR",
-    [ enum_LIB_SYS_LOGGING_LEVEL_WARNING ]  = "WARNING",
-    [ enum_LIB_SYS_LOGGING_LEVEL_NOTICE ]   = "NOTICE",
-    [ enum_LIB_SYS_LOGGING_LEVEL_INFO ]     = "INFO",
-    [ enum_LIB_SYS_LOGGING_LEVEL_DEBUG ]    = "DEBUG",
-    [ enum_LIB_SYS_LOGGING_LEVEL_TRACE ]    = "TRACE",
+const char \
+lib_sys_logging_level_strings[ e_LIB_SYS_LOGGING_LEVEL_LIMIT ][ 8U ] = {
+    [ e_LIB_SYS_LOGGING_LEVEL_NONE ]     = "NONE",
+    [ e_LIB_SYS_LOGGING_LEVEL_FATAL ]    = "FATAL",
+    [ e_LIB_SYS_LOGGING_LEVEL_ERROR ]    = "ERROR",
+    [ e_LIB_SYS_LOGGING_LEVEL_WARNING ]  = "WARNING",
+    [ e_LIB_SYS_LOGGING_LEVEL_NOTICE ]   = "NOTICE",
+    [ e_LIB_SYS_LOGGING_LEVEL_INFO ]     = "INFO",
+    [ e_LIB_SYS_LOGGING_LEVEL_DEBUG ]    = "DEBUG",
+    [ e_LIB_SYS_LOGGING_LEVEL_TRACE ]    = "TRACE",
 };
 
-struct s_lib_sys_logging_status
-lib_sys_logging_status = {
-    enum_LIB_SYS_LOGGING_STATUS_EMPTY,
-    LIB_SYS_LOGGING_LEVEL_LOCAL
-};
+int16_t lib_sys_logging_init(
+    void( *local_target )( uint8_t*, size_t ),
+    void( *remote_target )( uint8_t*, size_t )
+    )
+{
+    bool did_we_update_anything = false;
+    if ( NULL != local_target )
+    {
+        lib_sys_logging_status.local_output_target = local_target;
+        did_we_update_anything = true;
+    }
+    if ( NULL != remote_target )
+    {
+        lib_sys_logging_status.remote_output_target = remote_target;
+        did_we_update_anything = true;
+    }
+    if ( true == did_we_update_anything )
+    {
+        return ( 0 ); /* TODO: replace with success */
+    }
+    else
+    {
+        return ( 1 ); /* TODO: replace with invalid args error */
+    }
+}
 
-enum e_lib_sys_logging_status
-lib_sys_logging_get_threshold_local(
-    enum e_lib_sys_logging_level*       level
+int16_t lib_sys_logging_get_threshold_local(
+    enum e_lib_sys_logging_level* level
     )
 {
     if ( NULL != level )
     {
         *level = lib_sys_logging_status.level_threshold_local;
-        return ( lib_sys_logging_status.status );
+        return ( 0 ); /* TODO: replace with success */
     }
     else
     {
-        return ( enum_LIB_SYS_LOGGING_STATUS_ERROR );
+        return ( 1 ); /* TODO: replace with invalid arg error */
     }
 }
 
-enum e_lib_sys_logging_status
-lib_sys_logging_set_threshold_local(
-    enum e_lib_sys_logging_level        level
+int16_t lib_sys_logging_set_threshold_local(
+    enum e_lib_sys_logging_level level
     )
 {
-    if ( ( level > enum_LIB_SYS_LOGGING_LEVEL_NONE )
-         && ( level < enum_LIB_SYS_LOGGING_LEVEL_LIMIT )
+    if ( ( level >= e_LIB_SYS_LOGGING_LEVEL_NONE )
+         && ( level < e_LIB_SYS_LOGGING_LEVEL_LIMIT )
        )
     {
         lib_sys_logging_status.level_threshold_local = level;
-        return ( lib_sys_logging_status.status );
+        return ( 0 ); /* TODO: replace with success */
     }
-    else
-    {
-        return ( enum_LIB_SYS_LOGGING_STATUS_ERROR );
-    }
+    return ( 1 ); /* TODO: replace with invalid arg error */
 }
 
-enum e_lib_sys_logging_status
-lib_sys_logging_set_log(
-    enum e_lib_sys_logging_level        level,
-    char*                               buffer,
+int16_t lib_sys_logging_get_threshold_remote(
+    enum e_lib_sys_logging_level* level
+    )
+{
+    if ( NULL != level )
+    {
+        *level = lib_sys_logging_status.level_threshold_remote;
+        return ( 0 ); /* TODO: replace with success */
+    }
+    return ( 1 ); /* TODO: replace with invalid arg error */
+}
+
+int16_t lib_sys_logging_set_threshold_remote(
+    enum e_lib_sys_logging_level level
+    )
+{
+    if ( ( level >= e_LIB_SYS_LOGGING_LEVEL_NONE )
+         && ( level < e_LIB_SYS_LOGGING_LEVEL_LIMIT )
+       )
+    {
+        lib_sys_logging_status.level_threshold_remote = level;
+        return ( 0 ); /* TODO: replace with success */
+    }
+    return ( 1 ); /* TODO: replace with invalid arg error */
+}
+
+int16_t lib_sys_logging_set_log(
+    enum e_lib_sys_logging_level level,
+    char* buffer,
     ...
     )
 {
+    /* validate incoming message and some valid target/need for output */
     if ( ( NULL != buffer )
-         && ( level > enum_LIB_SYS_LOGGING_LEVEL_NONE )
-         && ( level < enum_LIB_SYS_LOGGING_LEVEL_LIMIT )
+         && ( level > e_LIB_SYS_LOGGING_LEVEL_NONE )
+         && ( level < e_LIB_SYS_LOGGING_LEVEL_LIMIT )
+         && ( ( lib_sys_logging_status.level_threshold_local <= level )
+              || ( lib_sys_logging_status.level_threshold_remote <= level )
+            )
+         && ( ( NULL != lib_sys_logging_status.local_output_target )
+              || ( NULL != lib_sys_logging_status.remote_output_target )
+            )
        )
     {
-        char temp1_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE ] = { '\0' };
-        char temp2_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE ] = { '\0' };
+        uint8_t temp1_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE - HEADER ] = { '\0' };
+        uint8_t temp2_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE ] = { '\0' };
 
         va_list args;
         va_start( args, buffer );
-        vsnprintf( temp1_buffer, LIB_SYS_LOGGING_MESSAGE_SIZE, buffer, args );
+        vsnprintf( ( char *)temp1_buffer, LIB_SYS_LOGGING_MESSAGE_SIZE, buffer, args );
         va_end( args );
 
-        snprintf( temp2_buffer,
-                  LIB_SYS_LOGGING_MESSAGE_SIZE,
-                  "%llu|%s|%s\r\n",
-                  hal_get_sys_tick(),
-                  lib_sys_logging_level_strings[ level ],
-                  temp1_buffer
-                  );
+        snprintf( ( char *)temp2_buffer,
+                LIB_SYS_LOGGING_MESSAGE_SIZE,
+                "%llu|%s|%s\r\n",
+                hal_get_sys_tick(),
+                lib_sys_logging_level_strings[ level ],
+                temp1_buffer
+                );
 
-        hal_cli_output_write( temp2_buffer, strlen( temp2_buffer ) );
+        temp2_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE - 1U ] = '\0';
+        temp2_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE - 2U ] = '.';
+        temp2_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE - 3U ] = '.';
+        temp2_buffer[ LIB_SYS_LOGGING_MESSAGE_SIZE - 4U ] = '.';
 
-        return ( lib_sys_logging_status.status );
+        if ( ( lib_sys_logging_status.level_threshold_local >= level )
+             && ( NULL != lib_sys_logging_status.local_output_target )
+           )
+        {
+            lib_sys_logging_status.local_output_target( temp2_buffer,
+                                                        strlen( temp2_buffer )
+                                                      );
+        }
+        if ( ( lib_sys_logging_status.level_threshold_remote >= level )
+             && ( NULL != lib_sys_logging_status.remote_output_target )
+           )
+        {
+            lib_sys_logging_status.remote_output_target( temp2_buffer,
+                                                         strlen( temp2_buffer )
+                                                       );
+        }
+        return ( 0 ); /*  TODO: replace with success */
     }
-    else
-    {
-        return ( enum_LIB_SYS_LOGGING_STATUS_ERROR );
-    }
+    return ( 1 ); /* TODO: replace with invalid arg error */
 }
 
 #ifdef DEPLOYMENT_OPTION_RUN_UNIT_TESTS
 
 #include <stdbool.h>
 
+void our_local_target( uint8_t* buffer, size_t length )
+{
+    /* TODO: how do we test that we made it here with proper data */
+}
+
+void our_remote_target( uint8_t* buffer, size_t length )
+{
+    /* TODO: how do we test that we made it here with proper data */
+}
+
 /* UNIT TEST function needed to complete unit testing of this library */
 bool run_unit_tests__lib_sys_logging( void )
 {
-    enum e_lib_sys_logging_status return_value;
+    int16_t ret;
     char buffer[] = "Hello World!";
     enum e_lib_sys_logging_level test_level;
 
-    /* TEST LEVEL THRESHOLD - test read and change of level threshold */
-    return_value = lib_sys_logging_get_threshold_local(
-            &test_level
-            ); /* test read of stored local level threshold */
-    if ( ( lib_sys_logging_status.status != return_value )
+    /* TEST LOCAL LEVEL THRESHOLD - test read and change of level threshold */
+    ret = lib_sys_logging_get_threshold_local( &test_level );
+    /* test read of stored local level threshold */
+    if ( ( 1U == ret )
          || ( lib_sys_logging_status.level_threshold_local != test_level )
        )
     {
         return ( false );
     }
 
-    return_value = lib_sys_logging_set_threshold_local(
-            enum_LIB_SYS_LOGGING_LEVEL_TRACE
-            ); /* test change of stored local level threshold */
-    if ( ( lib_sys_logging_status.status != return_value )
-         || ( lib_sys_logging_status.level_threshold_local != enum_LIB_SYS_LOGGING_LEVEL_TRACE )
+    ret = lib_sys_logging_set_threshold_local( e_LIB_SYS_LOGGING_LEVEL_DEBUG );
+    /* test change of stored local level threshold */
+    if ( ( 1U == ret )
+         || ( lib_sys_logging_status.level_threshold_local != e_LIB_SYS_LOGGING_LEVEL_DEBUG )
+       )
+    {
+        return ( false );
+    }
+
+    /* TEST REMOTE LEVEL THRESHOLD - test read and change of level threshold */
+    ret = lib_sys_logging_get_threshold_remote( &test_level );
+    /* test read of stored remote level threshold */
+    if ( ( 1U == ret )
+         || ( lib_sys_logging_status.level_threshold_remote != test_level )
+       )
+    {
+        return ( false );
+    }
+
+    ret = lib_sys_logging_set_threshold_remote( e_LIB_SYS_LOGGING_LEVEL_DEBUG );
+    /* test change of stored remote level threshold */
+    if ( ( 1U == ret )
+         || ( lib_sys_logging_status.level_threshold_remote != e_LIB_SYS_LOGGING_LEVEL_DEBUG )
        )
     {
         return ( false );
     }
 
     /* TEST LOGGING - with valid status, test sending a log */
-    return_value = lib_sys_logging_set_log(
-            enum_LIB_SYS_LOGGING_LEVEL_NONE,
-            buffer
-            );
-    if ( enum_LIB_SYS_LOGGING_STATUS_ERROR != return_value )
+    ret = lib_sys_logging_set_log( e_LIB_SYS_LOGGING_LEVEL_NOTICE, buffer );
+    /* this is valid but we have not set a target yet so we should fail */
+    if ( 1U != ret )
     {
         return ( false );
     }
 
-    return_value = lib_sys_logging_set_log(
-            enum_LIB_SYS_LOGGING_LEVEL_LIMIT,
-            buffer
-            );
-    if ( enum_LIB_SYS_LOGGING_STATUS_ERROR != return_value )
+    /* set a target for local and remote output testing */
+    ret = lib_sys_loggint_init( our_local_target, our_remote_target );
+    if ( ( 1U == ret )
+         || ( lib_sys_logging_status.local_output_target != our_local_target )
+         || ( lib_sys_logging_status.remote_output_target != our_remote_target )
+       )
     {
         return ( false );
     }
 
-    return_value = lib_sys_logging_set_log(
-            enum_LIB_SYS_LOGGING_LEVEL_NOTICE,
-            NULL
-            );
-    if ( enum_LIB_SYS_LOGGING_STATUS_ERROR != return_value )
+    ret = lib_sys_logging_set_log( e_LIB_SYS_LOGGING_LEVEL_NOTICE, buffer );
+    /* this is valid and we have now set a target so we should success */
+    if ( 1U == ret )
     {
         return ( false );
     }
 
-    return_value = lib_sys_logging_set_log(
-            enum_LIB_SYS_LOGGING_LEVEL_WARNING,
-            buffer
-            );
-    if ( lib_sys_logging_status.status != return_value )
+
+    ret = lib_sys_logging_set_log( e_LIB_SYS_LOGGING_LEVEL_trace, buffer );
+    /* this should return failure since TRACE is above current thresholds */
+    if ( 1U != ret )
+    {
+        return ( false );
+    }
+
+    ret = lib_sys_logging_set_log( e_LIB_SYS_LOGGING_LEVEL_NONE, buffer );
+    /* this should return failure since NONE is not valid level for new logs */
+    if ( 1U != ret )
+    {
+        return ( false );
+    }
+
+    ret = lib_sys_logging_set_log( e_LIB_SYS_LOGGING_LEVEL_LIMIT, buffer );
+    /* this should return failure since LIMIT is also not valid for new logs */
+    if ( 1U != ret )
+    {
+        return ( false );
+    }
+
+    ret = lib_sys_logging_set_log( e_LIB_SYS_LOGGING_LEVEL_NOTICE, NULL );
+    /* this should return failure since buffer is NULL and is nothing to log */
+    if ( 1U != ret )
     {
         return ( false );
     }
--- lib_sys_logging.h
+++ lib_sys_logging.h
@@ -1,83 +1,93 @@
 #ifndef LIB_SYS_LOGGING_H
 #define LIB_SYS_LOGGING_H
 
-/* =====================================================================================================================
- *                                                                                                   DEFAULT DEFINITIONS
- * =====================================================================================================================
+#include <stdint.h>
+
+/* =============================================================================
+ *                                                           DEFAULT DEFINITIONS
+ * =============================================================================
  */
-/* define LIB_SYS_LOGGING_LEVEL_LOCAL in deployment header to change the default threshold level of local logs
- * that are displayed.
+/* define LIB_SYS_LOGGING_LEVEL_LOCAL in makefile or pre-include deployment
+ * header to change default threshold level of local logs that are displayed
  */
 #ifndef LIB_SYS_LOGGING_LEVEL_LOCAL
-#define LIB_SYS_LOGGING_LEVEL_LOCAL enum_LIB_SYS_LOGGING_LEVEL_DEBUG
+#define LIB_SYS_LOGGING_LEVEL_LOCAL ( e_LIB_SYS_LOGGING_LEVEL_INFO )
 #warning "LIB_SYS_LOGGING: DEFAULT value used for LIB_SYS_LOGGING_LEVEL_LOCAL"
 #endif /* LIB_SYS_LOGGING_LEVEL_LOCAL */
 
-/* define LIB_SYS_LOGGING_MESSAGE_SIZE in deployment header to change the default string length limit when processing
- * and storing logs.
+/* define LIB_SYS_LOGGING_LEVEL_REMOTE in makefile or pre-include deployment
+ * header to change default threshold level of remote logs that are displayed.
+ */
+#ifndef LIB_SYS_LOGGING_LEVEL_REMOTE
+#define LIB_SYS_LOGGING_LEVEL_REMOTE ( e_LIB_SYS_LOGGING_LEVEL_WARNING )
+#warning "LIB_SYS_LOGGING: DEFAULT value used for LIB_SYS_LOGGING_LEVEL_REMOTE"
+#endif /* LIB_SYS_LOGGING_LEVEL_REMOTE */
+
+/* define LIB_SYS_LOGGING_MESSAGE_SIZE in makefile or pre-include deployment
+ * header to change default string length limit when processing and storing logs
+ * Strings longer than this limit will be cut short and end with "...".
  */
 #ifndef LIB_SYS_LOGGING_MESSAGE_SIZE
-#define LIB_SYS_LOGGING_MESSAGE_SIZE 128
+#define LIB_SYS_LOGGING_MESSAGE_SIZE ( ( uint8_t )128 )
 #warning "LIB_SYS_LOGGING: DEFAULT value used for LIB_SYS_LOGGING_MESSAGE_SIZE"
 #endif /* LIB_SYS_LOGGING_MESSAGE_SIZE */
 
-/* =====================================================================================================================
- *                                                                                           Enumerations and Structures
- * =====================================================================================================================
+/* =============================================================================
+ *                                                   Enumerations and Structures
+ * =============================================================================
  */
-/*                                                                                                     Library Status */
-enum e_lib_sys_logging_status
-{
-    enum_LIB_SYS_LOGGING_STATUS_ERROR = -1,
-    enum_LIB_SYS_LOGGING_STATUS_EMPTY = 0,
-    enum_LIB_SYS_LOGGING_STATUS_PENDING,
-    enum_LIB_SYS_LOGGING_STATUS_FULL,
-    enum_LIB_SYS_LOGGING_STATUS_LIMIT
-};
-
+/*                                                                Level Enums */
 enum e_lib_sys_logging_level
 {
-    enum_LIB_SYS_LOGGING_LEVEL_NONE = 0, /* set in case value is NULL */
-    enum_LIB_SYS_LOGGING_LEVEL_FATAL,
-    enum_LIB_SYS_LOGGING_LEVEL_ERROR,
-    enum_LIB_SYS_LOGGING_LEVEL_WARNING,
-    enum_LIB_SYS_LOGGING_LEVEL_NOTICE,
-    enum_LIB_SYS_LOGGING_LEVEL_INFO,
-    enum_LIB_SYS_LOGGING_LEVEL_DEBUG,
-    enum_LIB_SYS_LOGGING_LEVEL_TRACE,
-    enum_LIB_SYS_LOGGING_LEVEL_LIMIT
+    e_LIB_SYS_LOGGING_LEVEL_NONE = 0,
+    e_LIB_SYS_LOGGING_LEVEL_FATAL,       /* 3 a.m. call, unable to recover */
+    e_LIB_SYS_LOGGING_LEVEL_ERROR,       /* hotfix, recoverable, lost data */
+    e_LIB_SYS_LOGGING_LEVEL_WARNING,     /* helps guide next developments */
+    e_LIB_SYS_LOGGING_LEVEL_NOTICE,      /* added for remote statistics */
+    e_LIB_SYS_LOGGING_LEVEL_INFO,        /* major milestones, normal ops */
+    e_LIB_SYS_LOGGING_LEVEL_DEBUG,       /* major forks in logic */
+    e_LIB_SYS_LOGGING_LEVEL_TRACE,       /* details with supporting args */
+    e_LIB_SYS_LOGGING_LEVEL_LIMIT
 };
 
 struct s_lib_sys_logging_status
 {
-    enum e_lib_sys_logging_status status;
     enum e_lib_sys_logging_level level_threshold_local;
-    void( *local_output_target )( char*, size_t );
+    void( *local_output_target )( uint8_t*, size_t );
+    enum e_lib_sys_logging_level level_threshold_remote;
+    void( *remote_output_target )( uint8_t*, size_t );
 };
 
-enum e_lib_sys_logging_status
-lib_sys_logging_get_threshold_local(
-    enum e_lib_sys_logging_level*       level
+int16_t lib_sys_logging_init(
+    void( *local_target )( uint8_t*, size_t ),
+    void( *remote_target )( uint8_t*, size_t )
     );
 
-enum e_lib_sys_logging_status
-lib_sys_logging_set_threshold_local(
-    enum e_lib_sys_logging_level        level
+int16_t lib_sys_logging_get_threshold_local(
+    enum e_lib_sys_logging_level* level
     );
 
-enum e_lib_sys_logging_status
-lib_sys_logging_set_log(
-    enum e_lib_sys_logging_level        level,
-    char*                               buffer,
+int16_t lib_sys_logging_set_threshold_local(
+    enum e_lib_sys_logging_level level
+    );
+
+int16_t lib_sys_logging_get_threshold_remote(
+    enum e_lib_sys_logging_level* level
+    );
+
+int16_t lib_sys_logging_set_threshold_remote(
+    enum e_lib_sys_logging_level level
+    );
+
+int16_t lib_sys_logging_set_log(
+    enum e_lib_sys_logging_level level,
+    char* buffer,
     ...
     );
 
 #ifdef DEPLOYMENT_OPTION_RUN_UNIT_TESTS
 #include <stdbool.h>
-bool
-run_unit_tests__lib_sys_logging(
-    void
-    );
+bool run_unit_tests__lib_sys_logging( void );
 #endif /* DEPLOYMENT_OPTION_RUN_UNIT_TESTS */
 
 #endif /* LIB_SYS_LOGGING_H */
